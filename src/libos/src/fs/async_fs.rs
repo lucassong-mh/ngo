12c102e496b87f7d44d84397a080955bca4d9f86
LI Qing <geding.lq@antgroup.com>
2022-05-19 14:55:50 +0800

    [async-fs] Use SyncIoDisk for async_sfs

--- src/libos/src/fs/async_fs.rs
+++ src/libos/src/fs/async_fs.rs
@@ -3,7 +3,7 @@ use super::*;
 use async_once::AsyncOnce;
 use async_sfs::AsyncSimpleFS;
 use block_device::{BlockDevice, BLOCK_SIZE};
-use sgx_disk::{HostDisk, IoUringDisk};
+use sgx_disk::{HostDisk, IoUringDisk, SyncIoDisk};
 use std::path::{Path, PathBuf};
 use std::untrusted::path::PathEx;
 
@@ -15,13 +15,15 @@ lazy_static! {
     pub static ref ASYNC_SFS: AsyncOnce<Arc<dyn AsyncFileSystem>> = AsyncOnce::new(async{
         let image_path = PathBuf::from(ASYNC_SFS_IMAGE_PATH);
         let async_sfs = if image_path.exists() {
-            let iouring_disk = IoUringDisk::<runtime::IoUringRuntime>::open(&image_path).unwrap();
-            AsyncSimpleFS::open(Arc::new(iouring_disk)).await.unwrap()
+            //let iouring_disk = IoUringDisk::<runtime::IoUringRuntime>::open(&image_path).unwrap();
+            let sync_disk = SyncIoDisk::open(&image_path).unwrap();
+            AsyncSimpleFS::open(Arc::new(sync_disk)).await.unwrap()
         } else {
             const GB: usize = 1024 * 1024 * 1024;
-            let total_blocks = 4 * GB / BLOCK_SIZE;
-            let iouring_disk = IoUringDisk::<runtime::IoUringRuntime>::create_new(&image_path, total_blocks).unwrap();
-            AsyncSimpleFS::create(Arc::new(iouring_disk)).await.unwrap()
+            let total_blocks = 8 * GB / BLOCK_SIZE;
+            //let iouring_disk = IoUringDisk::<runtime::IoUringRuntime>::create_new(&image_path, total_blocks).unwrap();
+            let sync_disk = SyncIoDisk::create_new(&image_path, total_blocks).unwrap();
+            AsyncSimpleFS::create(Arc::new(sync_disk)).await.unwrap()
         };
         async_sfs as _
     });
